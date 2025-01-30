85ea9c932022479ec414fade33e9a71220bf2374
Raksha Kak <ridhika.kak2011@gmail.com>
2025-01-30 21:10:51 +0100

    FAQ Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import ProductCatalog from "./Pages/ProductCatalog";
 import NotFoundPage from "./Pages/404";
 import ProductDetail from "./Pages/ProductDetail";
 import ShoppingCart from "./Pages/ShoppingCart";
+import FAQ from "./Pages/FAQs";
 
 const App = () => {
 	return (
@@ -12,6 +13,7 @@ const App = () => {
 				<Route path="/" element={<ProductCatalog />} />
 				<Route path="/product/:id" element={<ProductDetail />} />
 				<Route path="/cart" element={<ShoppingCart />} />
+				<Route path="/faq" element={<FAQ />} />
 				<Route path="*" element={<NotFoundPage />} />
 			</Routes>
 		</Router>
