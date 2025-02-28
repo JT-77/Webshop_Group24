94ae3e50ebd569aa0889f1e2a7a3219ca4d91433
inevitable2103 <prpareek21@gmail.com>
2025-02-28 19:47:03 +0100

    Update settings.py

--- backend/cloudbackend/cloudbackend/settings.py
+++ backend/cloudbackend/cloudbackend/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 
 
 # For production (static files collected via collectstatic):
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
